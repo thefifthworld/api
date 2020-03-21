253ef7ce91e879c58b060500c5d2f77635e1af13
Jason Godesky <jefgodesky@gmail.com>
2020-03-21 14:54:39 -0400

    Unit test GET /

---
+++ api.spec.js
@@ -0,0 +1,19 @@
+/* global describe, it, expect */
+
+const request = require('supertest')
+const db = require('./db')
+const api = require('./api')
+
+describe('API', () => {
+  describe('GET /', () => {
+    it('should return 200', async () => {
+      const res = await request(api).get('/')
+      expect(res.status).toEqual(200)
+    })
+
+    it('should provide the number of pages', async () => {
+      const res = await request(api).get('/')
+      expect(res.body.pages).not.toBeNaN()
+    })
+  })
+})
