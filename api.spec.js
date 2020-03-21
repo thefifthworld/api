ee292896ab65dbce8ef7d7bb7312f46850b914f5
Jason Godesky <jefgodesky@gmail.com>
2020-03-21 15:31:10 -0400

    Test endpoint security

--- api.spec.js
+++ api.spec.js
@@ -3,6 +3,7 @@
 const request = require('supertest')
 const db = require('./db')
 const api = require('./api')
+const testUtils = require('./test-utils')
 
 describe('API', () => {
   describe('GET /', () => {
@@ -14,6 +15,27 @@ describe('API', () => {
     it('should provide the number of pages', async () => {
       const res = await request(api).get('/')
       expect(res.body.pages).not.toBeNaN()
+    })
+  })
+
+  describe('GET /secure', () => {
+    it('returns 401 when you don\'t provide basic auth', async () => {
+      const res = await request(api).get('/secure')
+      expect(res.status).toEqual(401)
+    })
+
+    it('returns 200 when you provide basic auth', async () => {
+      await testUtils.populateMembers(db)
+      const res = await request(api).get('/secure').auth('admin@thefifthworld.com', 'adminapikey000')
+      expect(res.status).toEqual(200)
+      await testUtils.resetTables(db, 'members')
+    })
+
+    it('returns 401 when you provide basic auth with wrong key', async () => {
+      await testUtils.populateMembers(db)
+      const res = await request(api).get('/secure').auth('admin@thefifthworld.com', 'nope')
+      expect(res.status).toEqual(401)
+      await testUtils.resetTables(db, 'members')
     })
   })
 })
