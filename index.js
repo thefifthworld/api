9b8c1934f30d606a0c14361f996bb275887fb806
Jason Godesky <jefgodesky@gmail.com>
2020-03-21 13:22:01 -0400

    Only return response if email and API key are given over basic auth

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const basicAuth = require('express-basic-auth')
+
 const db = require('./db')
 const config = require('./config')
 
 const app = express()
 const router = express.Router()
+
+const authorizer = async (email, key, cb) => {
+  const check = await db.run(`SELECT id, name, email, admin FROM members WHERE email='${email}' AND apikey='${key}' AND active=1;`)
+  if (check.length > 0) {
+    return cb(null, true)
+  } else {
+    return cb(null, false)
+  }
+}
+
+app.use(basicAuth( {
+  authorizer,
+  authorizeAsync: true,
+  challenge: true
+}))
 
 router.get('/', async (req, res) => {
   const query = await db.run('SELECT COUNT(id) AS count FROM pages;')
