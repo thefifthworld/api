db796969aace5d280ff3244fa894b0ca25af8804
Jason Godesky <jefgodesky@gmail.com>
2020-03-21 14:54:16 -0400

    Separate API and what runs it to make it more testable

--- index.js
+++ index.js
@@ -1,43 +1,7 @@
-const express = require('express')
-const basicAuth = require('express-basic-auth')
-
-const db = require('./db')
+const api = require('./api')
 const config = require('./config')
 
-const app = express()
-const router = express.Router()
-
-const authorizer = async (email, key, cb) => {
-  const check = await db.run(`SELECT id, name, email, admin FROM members WHERE email='${email}' AND apikey='${key}' AND active=1;`)
-  if (check.length > 0) {
-    return cb(null, true)
-  } else {
-    return cb(null, false)
-  }
-}
-
-const unauthorizedResponse = () => {
-  return { err: 'Unauthorized' }
-}
-
-const secure = basicAuth({
-  authorizer,
-  unauthorizedResponse,
-  authorizeAsync: true,
-  challenge: true
-})
-
-router.get('/', async (req, res) => {
-  const query = await db.run('SELECT COUNT(id) AS count FROM pages;')
-  res.status(200).json({ pages: query[0].count })
-})
-
-router.get('/secure', secure, (req, res) => {
-  res.status(200).json('hello world')
-})
-
 const { port } = config
-app.use('/', router)
-app.listen(port, () => {
+api.listen(port, () => {
   console.log(`The Fifth World API is listening on port ${port}`)
 })
