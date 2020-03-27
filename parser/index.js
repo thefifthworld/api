2007f32fa9aaa76f3f06023fbdc6a2cc0c5f3886
Jason Godesky <jefgodesky@gmail.com>
2020-03-27 17:16:29 -0400

    parseTags

--- parser/index.js
+++ parser/index.js
@@ -1,17 +1,55 @@
 const { Remarkable } = require('remarkable')
 
 /**
+ * Parse tags from string
+ * @param str {string} - Markdown to parse.
+ * @returns {Object} - an object with two properties:
+ *   - `stripped`: A copy of the original string with the tags removed.
+ *   - `tags`: An object that represents each tag with a property. The tag
+ *       is reduced to lower-case to use as the key. The value is an array of
+ *       all the values associated with that tag in the text. So, for example,
+ *       given a text like `[[Tag:1]] [[Tag:2]]`, then `tag` will be
+ *       `{ tag: [ '1', '2' ] }` (this is a parser, so all values are strings).
+ */
+
+const parseTags = str => {
+  const tags = {}
+  let stripped = str
+  const matches = str.match(/\[\[(.*?):(.*?)\]\]/gm)
+  if (matches) {
+    matches.forEach(match => {
+      stripped = stripped.replace(match, '')
+      const pair = match.substr(2, match.length - 4).split(':').map(el => el.trim())
+      if (pair && pair.length > 1) {
+        const key = pair[0].toLowerCase()
+        if (tags[key]) {
+          tags[key].push(pair[1])
+        } else {
+          tags[key] = [ pair[1] ]
+        }
+      }
+    })
+  }
+  stripped = stripped.replace(/ +/gm, ' ')
+  return { stripped, tags }
+}
+
+/**
  * Parse markdown.
  * @param str {string} - The markdown to parse.
- * @returns {Promise<{html: (string)}>} - An object detailing the results of
- *   the parsing. It has the following properties:
+ * @returns {Promise<Object>} - An object detailing the results of the parsing.
+ *   It has the following properties:
  *     - `html`: The HTML rendered from the markdown.
+ *     - `tags`: An object with the tags found in the string. Each tag becomes
+ *         a lower-case property key, with the value of that property being an
+ *         array of all the values assigned to that tag throughout the string.
  */
 
 const parser = async (str) => {
   const md = new Remarkable()
-  let html = md.render(str)
-  return { html }
+  const { stripped, tags } = parseTags(str)
+  let html = md.render(stripped)
+  return { html, tags }
 }
 
 module.exports = parser
--- parser/index.spec.js
+++ parser/index.spec.js
@@ -10,4 +10,10 @@ describe('Parser', () => {
     const actual = await parser('*Hello* **[world](https://thefifthworld.com)**', db)
     expect(actual.html).toEqual('<p><em>Hello</em> <strong><a href="https://thefifthworld.com">world</a></strong></p>\n')
   })
+
+  it('finds tags', async () => {
+    const actual = await parser('This [[Hello:World]] is [[Hello : Test]] text [[Tag: 1]] outside of tags.\n\nAnd here is a [[Test:true]] second paragraph.')
+    expect(actual.html).toEqual('<p>This is text outside of tags.</p>\n<p>And here is a second paragraph.</p>\n')
+    expect(actual.tags).toEqual({ hello: [ 'World', 'Test' ], tag: [ '1' ], test: [ 'true' ] })
+  })
 })
