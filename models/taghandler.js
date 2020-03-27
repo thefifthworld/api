0a1fd130635f771226da83b51c70ca5703e625a6
Jason Godesky <jefgodesky@gmail.com>
2020-03-27 19:43:09 -0400

    TagHandler.add

--- models/taghandler.js
+++ models/taghandler.js
@@ -2,6 +2,21 @@ class TagHandler {
   constructor () {
     this.tags = {}
   }
+
+  /**
+   * Add a tag.
+   * @param tag {string} - The name of the tag to add.
+   * @param val {string} - The value of the tag to add.
+   */
+
+  add (tag, val) {
+    const key = tag.toLowerCase()
+    if (this.tags[key]) {
+      this.tags[key].push(val)
+    } else {
+      this.tags[key] = [ val ]
+    }
+  }
 }
 
 module.exports = TagHandler
--- models/taghandler.spec.js
+++ models/taghandler.spec.js
@@ -9,4 +9,31 @@ describe('TagHandler', () => {
       expect(actual.tags).toEqual({})
     })
   })
+
+  describe('add', () => {
+    it('adds a tag', () => {
+      const actual = new TagHandler()
+      actual.add('test', 'hello world')
+      expect(actual.tags.test).toEqual([ 'hello world' ])
+    })
+
+    it('makes tag all lower-case', () => {
+      const actual = new TagHandler()
+      actual.add('TeSt', 'hello world')
+      expect(actual.tags.test).toEqual([ 'hello world' ])
+    })
+
+    it('handles tags with spaces', () => {
+      const actual = new TagHandler()
+      actual.add('another Test', 'hello world')
+      expect(actual.tags['another test']).toEqual([ 'hello world' ])
+    })
+
+    it('adds to existing tags', () => {
+      const actual = new TagHandler()
+      actual.add('test', 'hello')
+      actual.add('test', 'world')
+      expect(actual.tags.test).toEqual([ 'hello', 'world' ])
+    })
+  })
 })
