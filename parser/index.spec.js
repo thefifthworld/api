db4dce39702f4ce7da22e3a918cfe1f5df0db9fa
Jason Godesky <jefgodesky@gmail.com>
2020-03-27 14:55:58 -0400

    parser: Throw in external link test for good measure

--- parser/index.spec.js
+++ parser/index.spec.js
@@ -7,7 +7,7 @@ describe('Parser', () => {
   afterAll(done => { db.close(done) })
 
   it('renders markdown', async () => {
-    const actual = await parser('*Hello* **world**', db)
-    expect(actual.html).toEqual('<p><em>Hello</em> <strong>world</strong></p>\n')
+    const actual = await parser('*Hello* **[world](https://thefifthworld.com)**', db)
+    expect(actual.html).toEqual('<p><em>Hello</em> <strong><a href="https://thefifthworld.com">world</a></strong></p>\n')
   })
 })
