e3ec2a4edc08e5a4f227daf0e6f9eed75557d73a
Jason Godesky <jefgodesky@gmail.com>
2020-03-28 09:25:38 -0400

    LinkHandler.add

--- models/linkhandler.js
+++ models/linkhandler.js
@@ -1,6 +1,37 @@
+const { escape } = require('sqlstring')
+
 class LinkHandler {
   constructor () {
     this.links = []
+  }
+
+  /**
+   * Add a link to the handler.
+   * @param str {string} - The string used to create a link. This is a wikitext
+   *   link, like `[[Title]]` or `[[/path]]`, which would like to a page with
+   *   a matching title or path, respectively. Links can also include a pipe,
+   *   as in `[[Title | Text]]` or `[[/path | Text]]`. The string before the
+   *   pipe is used to match the link (by either title or path), while what
+   *   follows is used as the text of the link.
+   * @param db {Pool} - The database connection.
+   * @returns {{path: (string), text: string, isNew: boolean}} - An object
+   *   representing the information parsed out of the link string and used to
+   *   create the link. The `path` property is the path that we're to link to,
+   *   the `text` property is the text of the link, and `isNew` is a boolean
+   *   indicating if the destination for this link does not yet exist.
+   */
+
+  async add (str, db) {
+    const pair = str.substr(2, str.length - 4).split('|').map(el => el.trim())
+    const check = await db.run(`SELECT title, path FROM pages WHERE title=${escape(pair[0])} OR path=${escape(pair[0])};`)
+    const found = check && check.length > 0
+    const link = {
+      text: pair[pair.length - 1],
+      path: found ? check[0].path : `/new?title=${encodeURIComponent(pair[0])}`,
+      isNew: !found
+    }
+    this.links.push(link)
+    return link
   }
 }
 
--- models/linkhandler.spec.js
+++ models/linkhandler.spec.js
@@ -1,6 +1,9 @@
 /* global describe, it, expect, afterAll */
 
 const db = require('../db')
+const testUtils = require('../test-utils')
+const Member = require('../models/member')
+const Page = require('../models/page')
 
 const LinkHandler = require('./linkhandler')
 
@@ -11,6 +14,22 @@ describe('LinkHandler', () => {
     it('prepares an object for storing tags', () => {
       const actual = new LinkHandler()
       expect(actual.links).toEqual([])
+    })
+  })
+
+  describe('add', () => {
+    it('returns link info', async () => {
+      const handler = new LinkHandler()
+      const actual = await handler.add('[[Link]]', db)
+      expect(actual).toEqual({ path: '/new?title=Link', text: 'Link', isNew: true })
+    })
+
+    it('returns link info for an existing link', async () => {
+      await testUtils.createTestPage(Page, Member, db)
+      const handler = new LinkHandler()
+      const actual = await handler.add('[[Test Page]]', db)
+      await testUtils.resetTables(db)
+      expect(actual).toEqual({ path: '/test-page', text: 'Test Page', isNew: false })
     })
   })
 })
