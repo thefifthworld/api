3450ec5f65c2c1f0a4b3e098b163af96626dfe71
Jason Godesky <jefgodesky@gmail.com>
2020-03-27 20:00:09 -0400

    TagHandler.save

--- models/taghandler.js
+++ models/taghandler.js
@@ -1,3 +1,5 @@
+const { escape } = require('sqlstring')
+
 class TagHandler {
   constructor () {
     this.tags = {}
@@ -15,6 +17,26 @@ class TagHandler {
       this.tags[key].push(val)
     } else {
       this.tags[key] = [ val ]
+    }
+  }
+
+  /**
+   * Save tags to the database.
+   * @param id {number} - The primary key of the page to associate these tags
+   *   with in the database.
+   * @param db {Pool} - The database connection.
+   * @returns {Promise} - A promise that resolves when the tags have been saved
+   *   to the database.
+   */
+
+  async save (id, db) {
+    const special = [ 'type', 'location' ]
+    const keys = Object.keys(this.tags).filter(key => !special.includes(key))
+    await db.run(`DELETE FROM tags WHERE page=${escape(id)};`)
+    for (const key of keys) {
+      for (const val of this.tags[key]) {
+        await db.run(`INSERT INTO tags (page, tag, value) VALUES (${escape(id)}, ${escape(key)}, ${escape(val)});`)
+      }
     }
   }
 }
--- models/taghandler.spec.js
+++ models/taghandler.spec.js
@@ -1,8 +1,15 @@
-/* global describe, it, expect */
+/* global describe, it, expect, afterAll */
 
+const db = require('../db')
+const testUtils = require('../test-utils')
+
+const Member = require('./member')
+const Page = require('./page')
 const TagHandler = require('./taghandler')
 
 describe('TagHandler', () => {
+  afterAll(() => { db.end() })
+
   describe('constructor', () => {
     it('prepares an object for storing tags', () => {
       const actual = new TagHandler()
@@ -34,6 +41,69 @@ describe('TagHandler', () => {
       actual.add('test', 'hello')
       actual.add('test', 'world')
       expect(actual.tags.test).toEqual([ 'hello', 'world' ])
+    })
+  })
+
+  describe('save', () => {
+    it('saves tags to the database', async () => {
+      expect.assertions(7)
+      await testUtils.populateMembers(db)
+      const editor = await Member.load(2, db)
+      const data = { title: 'Test', body: 'This is a test.' }
+      const page = await Page.create(data, editor, 'Initial text', db)
+      const tags = new TagHandler()
+      tags.add('test', 'hello')
+      tags.add('test', 'world')
+      tags.add('num', '3')
+      await tags.save(page.id, db)
+      const rows = await db.run(`SELECT * FROM tags WHERE page=${page.id};`)
+      await testUtils.resetTables(db, 'tags', 'changes', 'pages', 'members')
+      expect(rows).toHaveLength(3)
+      expect(rows[0].tag).toEqual('test')
+      expect(rows[0].value).toEqual('hello')
+      expect(rows[1].tag).toEqual('test')
+      expect(rows[1].value).toEqual('world')
+      expect(rows[2].tag).toEqual('num')
+      expect(rows[2].value).toEqual('3')
+    })
+
+    it('doesn\'t save specially-handled tags', async () => {
+      expect.assertions(1)
+      await testUtils.populateMembers(db)
+      const editor = await Member.load(2, db)
+      const data = { title: 'Test', body: 'This is a test.' }
+      const page = await Page.create(data, editor, 'Initial text', db)
+      const tags = new TagHandler()
+      tags.add('type', 'test')
+      tags.add('location', 'here')
+      await tags.save(page.id, db)
+      const rows = await db.run(`SELECT * FROM tags WHERE page=${page.id};`)
+      await testUtils.resetTables(db, 'tags', 'changes', 'pages', 'members')
+      expect(rows).toHaveLength(0)
+    })
+
+    it('updates all tags', async () => {
+      expect.assertions(3)
+      await testUtils.populateMembers(db)
+      const editor = await Member.load(2, db)
+      const data = { title: 'Test', body: 'This is a test.' }
+      const page = await Page.create(data, editor, 'Initial text', db)
+
+      const before = new TagHandler()
+      before.add('test', 'hello')
+      before.add('test', 'world')
+      before.add('num', '3')
+      await before.save(page.id, db)
+
+      const after = new TagHandler()
+      after.add('test', 'hello')
+      await after.save(page.id, db)
+
+      const rows = await db.run(`SELECT * FROM tags WHERE page=${page.id};`)
+      await testUtils.resetTables(db, 'tags', 'changes', 'pages', 'members')
+      expect(rows).toHaveLength(1)
+      expect(rows[0].tag).toEqual('test')
+      expect(rows[0].value).toEqual('hello')
     })
   })
 })
