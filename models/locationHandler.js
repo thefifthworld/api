a45183b99d1826962e69498f6ab58ea650299ca9
Jason Godesky <jefgodesky@gmail.com>
2020-04-02 19:56:15 -0400

    LocationHandler.setLat, LocationHandler.setLon
    They've got a lot in common.

--- models/locationHandler.js
+++ models/locationHandler.js
@@ -3,6 +3,76 @@ class LocationHandler {
     this.lat = lat
     this.lon = lon
   }
+
+  /**
+   * Can take either a decimal, or a string representation of a decimal, or a
+   * string representation of a latitude inn degrees, minutes, and seconds
+   * format, and returns the decimal value for that latitude.
+   * @param lat {number|string} - A representation of a latitude.
+   * @returns {number|boolean} - The decimal value for the latitude given, or
+   *   `false` if it isn't a latitude.
+   */
+
+  setLat (lat) {
+    this.lat = LocationHandler.convertLatLon(lat, 'lat')
+  }
+
+  /**
+   * Can take either a decimal, or a string representation of a decimal, or a
+   * string representation of a longitude inn degrees, minutes, and seconds
+   * format, and returns the decimal value for that longitude.
+   * @param lon {number|string} - A representation of a longitude.
+   * @returns {number|boolean} - The decimal value for the longitude given, or
+   *   `false` if it isn't a longitude.
+   */
+
+  setLon (lon) {
+    this.lon = LocationHandler.convertLatLon(lon, 'lon')
+  }
+
+  /**
+   * Converting latitude and longitude have a lot in common, so here's a common
+   * function to reduce repeated code.
+   * @param str {number|string} - A latitude or longitude, represented either as
+   *   a number, a string of a number, or a string with degrees, minutes, and
+   *   seconds.
+   * @param dir {string} - A string telling the function whether to read the
+   *   string as latitude (`lat`) or longitude (`lon`). Defaults to `lon`.
+   * @returns {boolean|number} - If the string can be parsed into a valid value,
+   *   that value is returned. If not, returns `false`.
+   */
+
+  static convertLatLon (str, dir = 'lon') {
+    let val = str
+    const min = dir === 'lat' ? -90 : -180
+    const max = dir === 'lat' ? 90 : 180
+    const regex = dir === 'lat'
+      ? /(\d+)[°|`](\s?(\d+)\'(\s?(\d+)\")?(\s?(\d+)\.(\d+)")?)?[N|S]/
+      : /(\d+)[°|`](\s?(\d+)\'(\s?(\d+)\")?(\s?(\d+)\.(\d+)")?)?[E|W]/
+    const check = dir === 'lat' ? 'S' : 'W'
+
+    if ((typeof val === 'number') && ((val > max) || (val < min))) {
+      val = false
+    } else if (typeof val === 'string') {
+      let parse = val.match(regex)
+      if (parse) {
+        let degrees = parse.length > 1 ? parseInt(parse[1]) : 0
+        let minutes = parse.length > 3 ? parseInt(parse[3]) : 0
+        let seconds = parse.length > 7 ? parse[7] : 0
+        if (seconds && parse.length > 8) seconds += '.' + parse[8]
+        seconds = parseFloat(seconds)
+
+        if (seconds) minutes += seconds / 60
+        if (minutes) degrees += minutes / 60
+        if (parse[0].indexOf(check) > -1) degrees = degrees * -1
+        val = degrees
+      } else {
+        parse = parseFloat(val)
+        return isNaN(parse) ? false : parse
+      }
+    }
+    return val
+  }
 }
 
 module.exports = LocationHandler
--- models/locationHandler.spec.js
+++ models/locationHandler.spec.js
@@ -6,14 +6,102 @@ describe('LocationHandler', () => {
   describe('constructor', () => {
     it('sets lat and lon', () => {
       const actual = new LocationHandler(40.441823, -80.012778)
-      expect(actual.lat).toBeCloseTo(40.441823)
-      expect(actual.lon).toBeCloseTo(-80.012778)
+      expect(actual.lat).toBeCloseTo(40.441823, 3)
+      expect(actual.lon).toBeCloseTo(-80.012778, 3)
     })
 
     it('defaults to null', () => {
       const actual = new LocationHandler()
       expect(actual.lat).toEqual(null)
       expect(actual.lon).toEqual(null)
+    })
+  })
+
+  describe('convertLat', () => {
+    it('will keep a decimal format latitude', () => {
+      const lat = 40.441810
+      const handler = new LocationHandler()
+      handler.setLat(lat)
+      expect(handler.lat).toBeCloseTo(lat, 3)
+    })
+
+    it('will turn a string into a decimal value', () => {
+      const lat = '40.441810'
+      const handler = new LocationHandler()
+      handler.setLat(lat)
+      expect(handler.lat).toBeCloseTo(40.441810, 3)
+    })
+
+    it('will turn a latitude in degrees, minutes, and seconds into a decimal value', () => {
+      const lat = '40°26\'30.5"N'
+      const handler = new LocationHandler()
+      handler.setLat(lat)
+      expect(handler.lat).toBeCloseTo(40.441794, 3)
+    })
+
+    it('works with ticks', () => {
+      const lat = '40`26\'30.5"N'
+      const handler = new LocationHandler()
+      handler.setLat(lat)
+      expect(handler.lat).toBeCloseTo(40.441794, 3)
+    })
+
+    it('works with just degrees and minutes', () => {
+      const lat = '40°26\'N'
+      const handler = new LocationHandler()
+      handler.setLat(lat)
+      expect(handler.lat).toBeCloseTo(40.4333, 3)
+    })
+
+    it('works with just degrees', () => {
+      const lat = '40°N'
+      const handler = new LocationHandler()
+      handler.setLat(lat)
+      expect(handler.lat).toBeCloseTo(40, 3)
+    })
+  })
+
+  describe('convertLon', () => {
+    it('will keep a decimal format latitude', () => {
+      const lon = -80.012770
+      const handler = new LocationHandler()
+      handler.setLon(lon)
+      expect(handler.lon).toBeCloseTo(lon, 3)
+    })
+
+    it('will turn a string into a decimal value', () => {
+      const lon = '-80.012770'
+      const handler = new LocationHandler()
+      handler.setLon(lon)
+      expect(handler.lon).toBeCloseTo(-80.012770, 3)
+    })
+
+    it('will turn a latitude in degrees, minutes, and seconds into a decimal value', () => {
+      const lon = '80°00\'46.0"W'
+      const handler = new LocationHandler()
+      handler.setLon(lon)
+      expect(handler.lon).toBeCloseTo(-80.012770, 3)
+    })
+
+    it('works with ticks', () => {
+      const lon = '80`00\'46.0"W'
+      const handler = new LocationHandler()
+      handler.setLon(lon)
+      expect(handler.lon).toBeCloseTo(-80.012770, 3)
+    })
+
+    it('works with just degrees and minutes', () => {
+      const lon = '80°00\'W'
+      const handler = new LocationHandler()
+      handler.setLon(lon)
+      expect(handler.lon).toBeCloseTo(-80.000, 3)
+    })
+
+    it('works with just degrees', () => {
+      const lon = '80°W'
+      const handler = new LocationHandler()
+      handler.setLon(lon)
+      expect(handler.lon).toBeCloseTo(-80.000, 3)
     })
   })
 })
