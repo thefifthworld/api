dd97ec03e55cc92bd3dee0aeac7398ad49da8bee
Jason Godesky <jefgodesky@gmail.com>
2020-04-19 12:50:42 -0400

    LikesHandler.add

--- models/likesHandler.js
+++ models/likesHandler.js
@@ -1,9 +1,28 @@
+const { escape } = require('sqlstring')
+const Member = require('./member')
+
 class LikesHandler {
   constructor (page, ids) {
     const givenPage = page && page.constructor && page.constructor.name === 'Page'
     this.id = givenPage && !isNaN(page.id) ? page.id : null
     this.path = givenPage && page.path ? page.path : null
     this.ids = Array.isArray(ids) && ids.length > 0 ? ids : []
+  }
+
+  /**
+   * Add a like.
+   * @param member {Member|number} - A Member instance or a member's ID number.
+   * @param db {Pool} - The database connection.
+   * @returns {Promise<void>} - A Promise that resolves once the like has been
+   *   saved to the database.
+   */
+
+  async add (member, db) {
+    const mid = member instanceof Member ? member.id : !isNaN(member) ? member : null
+    if (mid !== null && this.id && this.path) {
+      await db.run(`INSERT INTO likes (path, page, member) VALUES (${escape(this.path)}, ${this.id}, ${mid});`)
+      this.ids.push(mid)
+    }
   }
 }
 
--- models/likesHandler.spec.js
+++ models/likesHandler.spec.js
@@ -1,9 +1,15 @@
-/* global describe, it, expect */
+/* global describe, it, expect, afterAll */
+
+const db = require('../db')
+const testUtils = require('../test-utils')
 
 const LikesHandler = require('./likesHandler')
+const Member = require('./member')
 const Page = require('./page')
 
 describe('LikesHandler', () => {
+  afterAll(() => { db.end() })
+
   describe('constructor', () => {
     it('returns a LikesHandler instance', () => {
       const actual = new LikesHandler()
@@ -25,6 +31,34 @@ describe('LikesHandler', () => {
       const ids = [ 1, 2, 3, 4, 5 ]
       const actual = new LikesHandler(p, ids)
       expect(actual.ids).toEqual(ids)
+    })
+  })
+
+  describe('add', () => {
+    it('adds a like', async () => {
+      expect.assertions(5)
+      const page = await testUtils.createTestPage(Page, Member, db)
+      const likes = new LikesHandler(page)
+      await likes.add(1, db)
+      const rows = await db.run(`SELECT * FROM likes WHERE page=${page.id};`)
+      await testUtils.resetTables(db)
+      expect(likes.ids).toEqual([ 1 ])
+      expect(rows).toHaveLength(1)
+      expect(rows[0].id).toEqual(page.id)
+      expect(rows[0].path).toEqual(page.path)
+      expect(rows[0].member).toEqual(1)
+    })
+
+    it('can take a Member as an argument', async () => {
+      expect.assertions(2)
+      const page = await testUtils.createTestPage(Page, Member, db)
+      const member = await Member.load(3, db)
+      const likes = new LikesHandler(page)
+      await likes.add(member, db)
+      const rows = await db.run(`SELECT * FROM likes WHERE page=${page.id};`)
+      await testUtils.resetTables(db)
+      expect(likes.ids).toEqual([ member.id ])
+      expect(rows[0].member).toEqual(member.id)
     })
   })
 })
