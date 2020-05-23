519aaf67df4253e0188f4e9205aa5c1302d3465f
Jason Godesky <jefgodesky@gmail.com>
2020-05-23 14:44:47 -0400

    Page.save: Unit test for images and thumbnailing

--- models/page.spec.js
+++ models/page.spec.js
@@ -333,11 +333,35 @@ describe('Page', () => {
       const file = page && page.files && page.files.length > 0 ? page.files[0] : null
       const url = file ? FileHandler.getURL(file.name) : null
       const check = url ? await testUtils.checkURL(url) : { status: null }
-      await FileHandler.remove(file.name)
+      await FileHandler.remove(file.name, db)
       await testUtils.resetTables(db)
       expect(page.files).toHaveLength(1)
       expect(page.files[0].mime).toEqual('text/plain')
       expect(check.status).toEqual(200)
+    })
+
+    it('creates a thumbnail', async () => {
+      expect.assertions(5)
+      await testUtils.populateMembers(db)
+      const editor = await Member.load(2, db)
+      const data = {
+        title: 'Test page',
+        body: 'This is a test.',
+        files: { file: testUtils.mockJPEG() }
+      }
+      const page = await Page.create(data, editor, 'Initial text', db)
+      const file = page && page.files && page.files.length > 0 ? page.files[0] : null
+      const fileURL = file ? FileHandler.getURL(file.name) : null
+      const thumbURL = file ? FileHandler.getURL(file.thumbnail) : null
+      const checkFile = fileURL ? await testUtils.checkURL(fileURL) : { status: null }
+      const checkThumb = thumbURL ? await testUtils.checkURL(thumbURL) : { status: null }
+      await FileHandler.remove(file.name, db)
+      await testUtils.resetTables(db)
+      expect(page.files).toHaveLength(1)
+      expect(file.thumbnail).toContain('test.thumb')
+      expect(file.mime).toEqual('image/jpeg')
+      expect(checkFile.status).toEqual(200)
+      expect(checkThumb.status).toEqual(200)
     })
   })
 
