28cbd4ce6424ec888208252e753ea8915d8fd37f
Jason Godesky <jefgodesky@gmail.com>
2020-12-19 10:33:36 -0500

    TemplateHandler.add - Add comment block documenting the function

--- models/templateHandler.js
+++ models/templateHandler.js
@@ -3,6 +3,13 @@ class TemplateHandler {
     this.templates = {}
   }
 
+  /**
+   * Add a template object.
+   * @param name {string} - The name of the template being used.
+   * @param obj {object} - Key/value pairs for any parameters used by the
+   *   template instance.
+   */
+
   add (name, obj) {
     this.templates[name] = Object.assign({}, obj)
   }
