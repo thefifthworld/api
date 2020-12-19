35e7ef5301d4eedbb059bbe3e718fa02120802b2
Jason Godesky <jefgodesky@gmail.com>
2020-12-19 10:29:28 -0500

    TemplateHandler.add

--- models/templateHandler.js
+++ models/templateHandler.js
@@ -2,6 +2,10 @@ class TemplateHandler {
   constructor () {
     this.templates = {}
   }
+
+  add (name, obj) {
+    this.templates[name] = Object.assign({}, obj)
+  }
 }
 
 module.exports = TemplateHandler
--- models/templateHandler.spec.js
+++ models/templateHandler.spec.js
@@ -9,4 +9,15 @@ describe('TemplateHandler', () => {
       expect(actual.templates).toEqual({})
     })
   })
+
+  describe('add', () => {
+    it('adds an object to a property name', () => {
+      const actual = new TemplateHandler()
+      actual.add('test', { a: 1, b: 2, c: 3 })
+      expect(actual.templates.test).toBeDefined()
+      expect(actual.templates.test.a).toEqual(1)
+      expect(actual.templates.test.b).toEqual(2)
+      expect(actual.templates.test.c).toEqual(3)
+    })
+  })
 })
