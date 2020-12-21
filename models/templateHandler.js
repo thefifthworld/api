2b3901e653a532355a4b4bce5811bec648eb8f6b
Jason Godesky <jefgodesky@gmail.com>
2020-12-20 19:32:04 -0500

    TemplateHandler.render: Render {{Novels}}

--- models/templateHandler.js
+++ models/templateHandler.js
@@ -259,6 +259,7 @@ class TemplateHandler {
           case 'Artists': renderings.push(this.renderArtists(instance, options, db)); break
           case 'Children': renderings.push(this.renderChildren(instance, options, db)); break
           case 'Gallery': renderings.push(this.renderChildren(instance, Object.assign({}, options, { asGallery: true }), db)); break
+          case 'Novels': renderings.push(this.renderNovels(instance, options, db)); break
           case 'Tagged': renderings.push(this.renderTagged(instance, options, db)); break
           default: renderings.push(this.renderDefault(template, instance, options, db)); break
         }
--- models/templateHandler.spec.js
+++ models/templateHandler.spec.js
@@ -370,6 +370,20 @@ describe('TemplateHandler', () => {
       expect(handler.instances.Gallery[0].markup).toEqual(expected)
     })
 
+    it('renders {{Novels}}', async () => {
+      expect.assertions(1)
+      await testUtils.populateMembers(db)
+      const editor = await Member.load(2, db)
+      const novel = await Page.create({ title: 'Children of Wormwood', body: '[[Type:Novel]]' }, editor, 'Initial text', db)
+      const cover = await Page.create({ title: 'Cover', body: '[[Type:Art]] [[Cover:Children of Wormwood]]', parent: novel.id }, editor, 'Initial text', db)
+      const art = new FileHandler({ name: 'cover.jpg', thumbnail: 'cover.thumb.jpg', mime: 'image/jpeg', size: 20000, page: cover.id, uploader: editor.id }); await art.save(db)
+      const handler = new TemplateHandler({ page: Page, fileHandler: FileHandler })
+      handler.add('Novels')
+      await handler.render({}, db)
+      await testUtils.resetTables(db)
+      expect(handler.instances.Novels[0].markup).toEqual(`<ul class="novel-listing"><li><a href="/children-of-wormwood"><img src="${FileHandler.getURL(art.name)}" alt="Children of Wormwood" /></a></li></ul>`)
+    })
+
     it('renders {{Tagged}}', async () => {
       expect.assertions(1)
       await testUtils.populateMembers(db)
